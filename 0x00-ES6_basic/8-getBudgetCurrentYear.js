432fa081179d9828b599ef45f4194330cbe6938c
tonnyCAPTAIN <tonnycaptain7@gmail.com>
2023-03-20 23:36:45 +0000

    final commit

--- 0x00-ES6_basic/8-getBudgetCurrentYear.js
+++ 0x00-ES6_basic/8-getBudgetCurrentYear.js
@@ -1,9 +1,12 @@
-export default function getBudgetObject (income, gdp, capita) {
-  const budget = {
-    income,
-    gdp,
-    capita,
-  };
+function getCurrentYear() {
+  const date = new Date();
+  return date.getFullYear();
+}
 
-  return budget;
+export default function getBudgetForCurrentYear(income, gdp, capita) {
+  return {
+    [`income-${getCurrentYear()}`]: income,
+    [`gdp-${getCurrentYear()}`]: gdp,
+    [`capita-${getCurrentYear()}`]: capita,
+  };
 }
